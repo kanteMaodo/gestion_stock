c845dd54880077c3d0760f6c4a40958566ccdd97
kantemaodo <kantemaodo@icloud.com>
2025-07-25 01:13:42 +0000

    first commit

---
+++ src/main/java/org/example/gestionpharmacie/model/TestConnection.java
@@ -0,0 +1,24 @@
+package org.example.gestionpharmacie.Models;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("PharmaPU");
+            EntityManager em = emf.createEntityManager();
+
+            System.out.println("✅ Connexion réussie à PostgreSQL !");
+            System.out.println("✅ Table 'utilisateurs' sera créée automatiquement");
+
+            em.close();
+            emf.close();
+        } catch (Exception e) {
+            System.err.println("❌ Erreur de connexion : " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
